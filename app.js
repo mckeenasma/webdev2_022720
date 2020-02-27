ef2b0708cf10994e86f6ee4ef32228c31b23003d
cbatuic <cbalaman@uic.edu.ph>
2020-02-27 11:42:21 +0800

    second commit

--- app.js
+++ app.js
@@ -25,5 +25,6 @@ app.get('/', function(req, res) {
 
     
 })
-app.listen(3000)
-console.log('Server is running on 192.168.202.106:3000')
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
